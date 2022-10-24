926dd873c351acd7152b12248962e08ee00ca3c5
hasi <hasithaera.pro@gmail.com>
2022-10-24 22:14:39 +0530

    Code that will list followers

--- main.py
+++ main.py
@@ -1,4 +1,154 @@
+import os
+from time import sleep
+from typing import Dict, List
 
-def main():
-    passa
-    pass
+from instagrapi import Client
+from instagrapi.types import UserShort
+
+IG_USERNAME = 'pasindusamaranayake'
+IG_PASSWORD = 'adalanane'
+IG_CREDENTIAL_PATH = './ig_settings.json'
+SLEEP_TIME = '600'  # in seconds
+
+
+class Bot:
+    _cl = None
+
+    def __init__(self):
+        self._cl = Client()
+        if os.path.exists(IG_CREDENTIAL_PATH):
+            self._cl.load_settings(IG_CREDENTIAL_PATH)
+            self._cl.login(IG_USERNAME, IG_PASSWORD)
+        else:
+            self._cl.login(IG_USERNAME, IG_PASSWORD)
+            self._cl.dump_settings(IG_CREDENTIAL_PATH)
+
+    def follow_by_username(self, username) -> bool:
+        """
+        Follow a user
+
+        Parameters
+        ----------
+        username: str
+            Username for an Instagram account
+
+        Returns
+        -------
+        bool
+            A boolean value
+        """
+        userid = self._cl.user_id_from_username(username)
+        return self._cl.user_follow(userid)
+
+    def unfollow_by_username(self, username) -> bool:
+        """
+        Unfollow a user
+
+        Parameters
+        ----------
+        username: str
+            Username for an Instagram account
+
+        Returns
+        -------
+        bool
+            A boolean value
+        """
+        userid = self._cl.user_id_from_username(username)
+        return self._cl.user_unfollow(userid)
+
+    def get_followers(self, amount: int = 0) -> Dict[int, UserShort]:
+        """
+        Get bot's followers
+
+        Parameters
+        ----------
+        amount: int, optional
+            Maximum number of media to return, default is 0 - Inf
+
+        Returns
+        -------
+        Dict[int, UserShort]
+            Dict of user_id and User object
+        """
+        return self._cl.user_followers(self._cl.user_id, amount=amount)
+
+    def get_followers_usernames(self, amount: int = 0) -> List[str]:
+        """
+        Get bot's followers usernames
+
+        Parameters
+        ----------
+        amount: int, optional
+            Maximum number of media to return, default is 0 - Inf
+
+        Returns
+        -------
+        List[str]
+            List of usernames
+        """
+        followers = self._cl.user_followers(self._cl.user_id, amount=amount)
+        return [user.username for user in followers.values()]
+
+    def get_following(self, amount: int = 0) -> Dict[int, UserShort]:
+        """
+        Get bot's followed users
+
+        Parameters
+        ----------
+        amount: int, optional
+            Maximum number of media to return, default is 0 - Inf
+
+        Returns
+        -------
+        Dict[int, UserShort]
+            Dict of user_id and User object
+        """
+        return self._cl.user_following(self._cl.user_id, amount=amount)
+
+    def get_following_usernames(self, amount: int = 0) -> List[str]:
+        """
+        Get bot's followed usernames
+
+        Parameters
+        ----------
+        amount: int, optional
+            Maximum number of media to return, default is 0 - Inf
+
+        Returns
+        -------
+        List[str]
+            List of usernames
+        """
+        following = self._cl.user_following(self._cl.user_id, amount=amount)
+        print("following")
+        return [user.username for user in following.values()]
+
+    def update(self):
+        """
+        Do something
+        """
+        pass
+
+
+if __name__ == '__main__':
+    # bot = Bot()
+    # user_id = bot.user_id_from_username("kimkardashian")
+    # list = bot.get_following_usernames(user_id,100)
+    # print (list)
+    # print(len(list))
+
+    cl = Client()
+    cl.login("pasindusamaranayake", "crowmaster201")
+
+    user_id = cl.user_id_from_username("kimkardashian")
+
+    print(user_id)
+    followers = cl.user_following(user_id, 7500)
+
+    count =0
+    for user in followers.values():
+        count+=1
+        print(f"Number {count}:={user.username}")
+
+
