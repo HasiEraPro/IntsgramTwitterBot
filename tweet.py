cc4d1e22cfa7bff81e644038bdc0a3c8c97287bf
HasiEraPro <hasithaera.pro@gmail.com>
2022-10-28 23:52:22 +0530

    working tweet api

--- tweet.py
+++ tweet.py
@@ -1,12 +1,26 @@
 import tweepy
 
-api_key = "..."
-api_secrets = "..."
-access_token = "..."
-access_secret = "..."
+# Your app's API/consumer key and secret can be found under the Consumer Keys
+# section of the Keys and Tokens tab of your app, under the
+# Twitter Developer Portal Projects & Apps page at
+# https://developer.twitter.com/en/portal/projects-and-apps
+consumer_key = "EWkP4LreABvdoPjC6cuQ8cTyq"
+consumer_secret = "yQQ5cIr7iGgdRtRFiyj1F4W1VaGKOBLkgPPOPT3BvnWUEqPGTt"
 
-# Authenticate to Twitter
-auth = tweepy.OAuthHandler(api_key, api_secrets)
-auth.set_access_token(access_token, access_secret)
+# Your account's (the app owner's account's) access token and secret for your
+# app can be found under the Authentication Tokens section of the
+# Keys and Tokens tab of your app, under the
+# Twitter Developer Portal Projects & Apps page at
+# https://developer.twitter.com/en/portal/projects-and-apps
+access_token = "835579733723205633-bEXZQZHANmkgV9G0H4Q0kwe9jHGLuyw"
+access_token_secret = "N0yvkKet5Roc5JB737AVQKsoIIuZCtdwzspdVy2Iy3iuA"
 
-tweetapi = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True)
+auth = tweepy.OAuth1UserHandler(
+    consumer_key, consumer_secret, access_token, access_token_secret
+)
+
+api = tweepy.API(auth)
+
+# If the authentication was successful, this should print the
+# screen name / username of the account
+print(api.verify_credentials().screen_name)
